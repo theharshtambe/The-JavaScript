1f602842c586a16b4122f1a286827c6a524d6561
_tambeharsh_ <harshtambe.dev@gmail.com>
2024-12-29 19:02:52 +0530

    completed two lessons

---
+++ TypeScript/src/01-introduction.ts
@@ -0,0 +1,13 @@
+/*
+Benefits of TS
+1. Better error feedback
+2. Better autocompletion & code hints
+3. Custom types
+4. Self documenting  
+*/
+
+function sayHello(name: string): void {
+   console.log(`Hello! ${name}`);
+}
+
+sayHello('Harsh');
---
+++ TypeScript/src/02-basic-types.ts
@@ -0,0 +1,18 @@
+// number, string & boolean
+const iAmNumber: number = 24;
+const iAmString: string = 'Harsh';
+const iAmBoolean: boolean = true;
+
+console.log(iAmNumber);
+console.log(iAmString);
+console.log(iAmBoolean);
+
+// null & undefined (don't have much use)
+let something: null;
+something = null;
+
+let anotherThing: undefined;
+anotherThing = undefined;
+
+console.log(null);
+console.log(anotherThing);
